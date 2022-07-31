2d96435ac6c7a870216da04470822f08b9688c1a
vXultz <vitorserta@gmail.com>
2022-07-31 18:14:26 -0300

    Login Screen

--- src/Pages/Home/HomeStyle.tsx
+++ src/Pages/Home/HomeStyle.tsx
@@ -2,64 +2,59 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  flex: 1;
+  min-height: 100vh;
+  background: #211D28;
+  justify-content: flex-start;
   flex-direction: column;
   align-items: center;
-  min-height: 100vh;
-  min-width: 100vw;
-  background-color: ${({ theme }) => theme.colors.secondary};
 `;
 
-export const MainContent = styled.main`
-  display: flex;
-  align-self: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100%;
-  background: linear-gradient(purple, transparent);
+export const Tittle = styled.h1`
+color: white;
 `;
 
-export const DonateForm = styled.form`
-  display: flex;
-  flex-direction: column;
+export const SubTittle = styled.h2`
+opacity: 0.5;
+color: #FFFFFF;
+`;
 
-  section:first-of-type {
-    justify-content: center;
-  }
+export const Input = styled.input`
+border: 2px solid #454141;
+border-radius: 8px 8px 0px 0px;
+background: transparent;
+min-width: 20rem;
+height: 2.5rem;
+color: #908e93;
+:hover {
+  border-color: #793fc3;
+}
+`;
 
-  section {
-    display: flex;
-    align-items: flex-end;
-    flex: 1;
-    width: 100%;
-    margin-top: 1rem;
-    flex-wrap: wrap;
-    justify-content: center;
-    margin: -0.5rem;
-    margin-top: 0.5rem;
-    & > * {
-      margin: 0.5rem;
-      @media screen and (max-width: 360px) {
-        margin-top: 1rem;
-      }
-    }
+export const InputBottom = styled(Input)`
+border-radius: 0 0 8px 8px;
+`;
 
-    & > button {
-      min-width: 4rem;
-      align-items: center;
-      align-self: stretch;
-      @media screen and (max-width: 360px) {
-        flex: 1;
-      }
-      &[type='submit'] {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 1.5rem;
-        & > svg {
-          fill: #fff;
-        }
-      }
-    }
-  }
+export const ContainerForgot = styled.button`
+min-width: 20.5rem;
+height: 3rem;
+display: flex;
+justify-content: flex-end;
+background: transparent;
+border: none;
+`;
+
+export const TextForgot = styled.h2`
+color: #F600DD;
+font-size: 1rem;
+`;
+
+
+export const ButtonLogin = styled.button`
+min-width: 20rem;
+min-height: 3rem;
+background: linear-gradient(135deg, #9A4DFF 0%, #F600DD 100%);
+box-shadow: 0px 4px 20px rgba(188, 49, 243, 0.5);
+border-radius: 8px;
+border: none;
+color: #FFFFFF;
 `;
