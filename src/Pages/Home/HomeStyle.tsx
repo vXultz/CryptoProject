26aa86a3bea6421ba1209a0e36397f442933c506
vXultz <vitorserta@gmail.com>
2022-08-06 17:04:26 -0300

    progress

--- src/Pages/Home/HomeStyle.tsx
+++ src/Pages/Home/HomeStyle.tsx
@@ -3,58 +3,93 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   min-height: 100vh;
-  background: #211D28;
+  background: #211d28;
   justify-content: flex-start;
   flex-direction: column;
   align-items: center;
 `;
 
 export const Tittle = styled.h1`
-color: white;
+  color: white;
 `;
 
 export const SubTittle = styled.h2`
-opacity: 0.5;
-color: #FFFFFF;
+  opacity: 0.5;
+  color: #ffffff;
 `;
 
 export const Input = styled.input`
-border: 2px solid #454141;
-border-radius: 8px 8px 0px 0px;
-background: transparent;
-min-width: 20rem;
-height: 2.5rem;
-color: #908e93;
-:hover {
-  border-color: #793fc3;
-}
+  border: 2px solid #454141;
+  border-radius: 8px 8px 0px 0px;
+  background: transparent;
+  min-width: 20rem;
+  height: 2.5rem;
+  color: #908e93;
+  :hover {
+    border-color: #793fc3;
+  }
 `;
 
 export const InputBottom = styled(Input)`
-border-radius: 0 0 8px 8px;
+  border-radius: 0 0 8px 8px;
 `;
 
 export const ContainerForgot = styled.button`
-min-width: 20.5rem;
-height: 3rem;
-display: flex;
-justify-content: flex-end;
-background: transparent;
-border: none;
+  min-width: 20.5rem;
+  height: 3rem;
+  display: flex;
+  justify-content: flex-end;
+  background: transparent;
+  border: none;
 `;
 
 export const TextForgot = styled.h2`
-color: #F600DD;
-font-size: 1rem;
+  color: #f600dd;
+  font-size: 1rem;
 `;
 
-
 export const ButtonLogin = styled.button`
-min-width: 20rem;
-min-height: 3rem;
-background: linear-gradient(135deg, #9A4DFF 0%, #F600DD 100%);
-box-shadow: 0px 4px 20px rgba(188, 49, 243, 0.5);
-border-radius: 8px;
-border: none;
-color: #FFFFFF;
+  min-width: 20rem;
+  min-height: 3rem;
+  background: linear-gradient(135deg, #9a4dff 0%, #f600dd 100%);
+  box-shadow: 0px 4px 20px rgba(188, 49, 243, 0.5);
+  border-radius: 8px;
+  border: none;
+  color: #ffffff;
+  cursor: pointer;
+`;
+
+export const Divisor = styled.div`
+  margin: 2rem;
+  width: 80%;
+  border: 1px solid rgba(252, 221, 236, 0.2);
+`;
+
+export const ButtonMetamask = styled(ButtonLogin)`
+  margin-bottom: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonInsideMetamask = styled.button`
+  background: #291d32;
+  min-width: 20.8rem;
+  min-height: 2.5rem;
+  border-radius: 4px;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  h1 {
+    margin: 0;
+    font-size: 1.2rem;
+    background: -webkit-linear-gradient(135deg, #9a4dff 0%, #f600dd 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  img {
+    margin-left: 1rem;
+  }
 `;
--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
 
 import * as style from './HomeStyle';
+import fox from '../../img/fox.png';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-  <style.Container>
-    <img 
-    src="https://imageup.me/images/a9dad55c-a6bd-4ff2-ae1c-eb774c788de4.png" alt="" 
-    width="200px"
-    height="200px"/>
-    <style.Tittle>
-      Crypto
-    </style.Tittle>
-    <style.SubTittle>
-      O seu marktplace de NFTs no Brasil
-    </style.SubTittle>
-    <style.Input defaultValue={"Login"} />
-    <style.InputBottom defaultValue={"Senha"} />
-    <style.ContainerForgot>
-      <style.TextForgot>Esqueceu sua senha?</style.TextForgot>
-    </style.ContainerForgot>
-    <style.ButtonLogin>Entrar</style.ButtonLogin>
+    <style.Container>
+      <img
+        src="https://imageup.me/images/a9dad55c-a6bd-4ff2-ae1c-eb774c788de4.png"
+        alt=""
+        width="200px"
+        height="200px"
+      />
+      <style.Tittle>Crypto</style.Tittle>
+      <style.SubTittle>O seu marktplace de NFTs no Brasil</style.SubTittle>
+      <style.Input placeholder={'Login'} />
+      <style.InputBottom placeholder={'Senha'} />
+      <style.ContainerForgot>
+        <style.TextForgot>Esqueceu sua senha?</style.TextForgot>
+      </style.ContainerForgot>
+
+      <Link to="/Feed">
+        <style.ButtonLogin>Entrar</style.ButtonLogin>
+      </Link>
+
+      <style.Divisor></style.Divisor>
+      <style.ButtonMetamask>
+        <style.ButtonInsideMetamask>
+          <h1>Entrar com a Metamask</h1>
+          <img src={fox} alt="" width="25px" height="25px" />
+        </style.ButtonInsideMetamask>
+      </style.ButtonMetamask>
     </style.Container>
   );
 }
